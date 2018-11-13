b2f851245a85864b507e7aefdfefa85a06dd1649
Unknown <yayapipi@hotmail.com>
2018-11-13 23:01:36 +0800

    Scene Change Crasher

---
+++ Shop VR Project/shop_UI_2018_2/Assets/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    public void changeScene(int i)
+    {
+        Application.LoadLevel(i);
+    }
+    public void changeScene(string i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
