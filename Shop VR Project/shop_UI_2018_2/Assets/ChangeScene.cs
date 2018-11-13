6c8c6706525d1f906499ffcc641a3b2df3745cb5
mmiikeke <o860116@gmail.com>
2018-11-14 06:16:52 +0800

    attempts to unload the currently loaded VRTK_SDKSetup

--- Shop VR Project/shop_UI_2018_2/Assets/ChangeScene.cs
+++ Shop VR Project/shop_UI_2018_2/Assets/ChangeScene.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using VRTK;
 
 public class ChangeScene : MonoBehaviour {
     public void changeScene(int i)
     {
-        Application.LoadLevel(i);
+        bool success;
+        success = VRTK_SDKManager.AttemptUnloadSDKSetup();
+        if (success)
+            Application.LoadLevel(i);
+        else
+            Debug.Log("not success");
     }
     public void changeScene(string i)
     {
-        SceneManager.LoadScene(i);
+        bool success;
+        success = VRTK_SDKManager.AttemptUnloadSDKSetup();
+        if (success)
+            SceneManager.LoadScene(i);
+        else
+            Debug.Log("not success");
+    }
+
+    private void unload()
+    {
+
     }
 }
