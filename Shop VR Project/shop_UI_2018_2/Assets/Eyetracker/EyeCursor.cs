3f3cb81905981c25fba71827a22c23b35fbd8244
mmiikeke <o860116@gmail.com>
2018-11-19 17:50:57 +0800

    修正eye cursor與用戶的距離
    1.eye cursor與用戶的距離設為eyecamera.forward 向量與 (rayhit - raypos)向量內積的值

--- Shop VR Project/shop_UI_2018_2/Assets/Eyetracker/EyeCursor.cs
+++ Shop VR Project/shop_UI_2018_2/Assets/Eyetracker/EyeCursor.cs
@@ -6,17 +6,23 @@ using aGlassDKII;
 public class EyeCursor : MonoBehaviour {
 	public GameObject cursor;
     public GameObject cursor2;
+    public Canvas cursorCanvas;
 	float x, y;
     void Start()
     {
+        cursorCanvas = cursor.transform.parent.GetComponent<Canvas>();
     }
 
 	void Update () {
-		//gaze.GetPos (lighter.gameObject, ref x, ref y);
+        //gaze.GetPos (lighter.gameObject, ref x, ref y);
         x = GameGaze.x;
         y = GameGaze.y;
-		cursor.transform.localPosition = new Vector2 ((x - 0.5f) * 1512, (0.5f - y) * 1680);
-        if(cursor2)
+        float dis = Vector3.Dot(MainController.currentPointerCamera.transform.forward, (MainController.Instance("EyeCursor").rayHitPos - MainController.currentPointerCamera.transform.position));
+        if (dis > 0)
+            cursorCanvas.planeDistance = dis;
+
+        cursor.transform.localPosition = new Vector2((x - 0.5f) * 1512, (0.5f - y) * 1680);
+        if (cursor2)
             cursor2.transform.localPosition = new Vector2((x - 0.5f) * 1512, (0.5f - y) * 1680);
     }
 }
